26960ece564591a905ad86426ffd6399d242fa93
Mikhail Kolmogorov <fenderglass@gmail.com>
2021-10-28 19:37:27 -0700

    pipeline is working

--- dipdiff.py
+++ dipdiff.py
@@ -13,13 +13,15 @@ MINIMAP2 = os.path.join(pipeline_dir, "submodules", "minimap2", "minimap2")
 SAMTOOLS = "samtools"
 
 sys.path.insert(0, os.path.join(pipeline_dir, "submodules", "svim-asm", "src"))
-SVIM = os.path.join(pipeline_dir, "submodules", "svim-asm", "src", "svim_asm", "svim-asm")
+import svim_asm.main as svim
 
 
 def generate_alignment(ref_path, asm_path, num_threads, out_bam):
-    subprocess.check_call("minimap2 -ax asm20 -B 2 -E 3,1 -O 6,100 --cs -t {0} {1} {2} -K 5G | samtools sort -m 4G -@ 8 >{3}"
-                            .format(num_threads, ref_path, asm_path, out_bam), shell=True, stderr=open(os.devnull, "w"))
-    subprocess.check_call("samtools index {0}".format(out_bam), shell=True)
+    cmd = "minimap2 -ax asm20 -B 2 -E 3,1 -O 6,100 --cs -t {0} {1} {2} -K 5G | samtools sort -m 4G -@ 8 >{3}" \
+                            .format(num_threads, ref_path, asm_path, out_bam)
+    print("Running: " + cmd)
+    subprocess.check_call(cmd, shell=True, stderr=open(os.devnull, "w"))
+    subprocess.check_call("samtools index -@ 4 {0}".format(out_bam), shell=True)
 
 
 def main():
@@ -45,19 +47,17 @@ def main():
                         default=10, metavar="int", help="number of parallel threads [10]")
     args = parser.parse_args()
 
-    for e in [MINIMAP2, SAMTOOLS, SVIM]:
+    for e in [MINIMAP2, SAMTOOLS]:
         if not spawn.find_executable(e):
             print("Not installed: " + e, file=sys.stderr)
             return 1
 
-    if not args.out_dir:
+    if not os.path.isdir(args.out_dir):
         os.mkdir(args.out_dir)
 
     prefix = "dipdiff"
     aln_1 = os.path.join(args.out_dir, prefix + "_pat" + ".bam")
     aln_2 = os.path.join(args.out_dir, prefix + "_mat" + ".bam")
-
-    print("Running minimap2")
 
     thread_1 = Thread(target=generate_alignment, args=(args.reference, args.hap_pat, args.threads // 2, aln_1))
     thread_2 = Thread(target=generate_alignment, args=(args.reference, args.hap_mat, args.threads // 2, aln_2))
@@ -66,8 +66,9 @@ def main():
     thread_1.join()
     thread_2.join()
 
-    svim_cmd = [SVIM, "diploid", args.out_dir, aln_1, aln_2, args.reference, "--min_sv_size", str(args.sv_size),
+    svim_cmd = ["diploid", args.out_dir, aln_1, aln_2, args.reference, "--min_sv_size", str(args.sv_size),
                 "--partition_max_distance", "5000", "--max_edit_distance", "0.3", "--filter_contained"]
+    svim.main(svim_cmd)
     #os.remove(os.path.join(out_dir, "sv-lengths.png"))
 
     return 0
